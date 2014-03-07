24de540b7c1ba56f27da58f22346eb2b2ecfff8f
Matt Hartley <mbhartley1490@gmail.com>
2014-03-06 19:40:20 -0500

    first contact saved to Parse cloud

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('\'Allo \'Allo!');
+//console.log('\'Allo \'Allo!');
+
+$(function(){
+ 
+  Parse.initialize("75BmfRTZaHEhjFi2LGcwwpBSs9R2lavh3hDCwQtd", "j2gNcLny1ubShHclT3cPIA6PH06SDYRolgk4mflU");
+
+  console.log ('parse initialize firing')
+
+  var Contact = Parse.Object.extend('Contact');
+
+  var contact = new Contact();
+
+  contact.set("contactName", "Matt Hartley");
+  contact.set("contactCity", "Greenville");
+  contact.set("email", "mbhartley1490@gmail.com");
+
+  //contact.save(null, {
+  //	success: function(contactList){
+  //	  console.log('New object created with objectID' + contact.id);
+  //	},
+  //	error: function(contactList, error){
+  //	  console.log('Failed to create new object, with error code' + error.description);	
+  //	}
+
+  //});
+
+})
+
