56f4275bb63afea465b0665f63727938c6d2471d
Matt Hartley <mbhartley1490@gmail.com>
2014-03-06 20:14:36 -0500

    corrected push issues

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,22 +2,26 @@
 
 $(function(){
  
- Parse.initialize("75BmfRTZaHEhjFi2LGcwwpBSs9R2lavh3hDCwQtd", "j2gNcLny1ubShHclT3cPIA6PH06SDYRolgk4mflU");
+  Parse.initialize("75BmfRTZaHEhjFi2LGcwwpBSs9R2lavh3hDCwQtd", "j2gNcLny1ubShHclT3cPIA6PH06SDYRolgk4mflU");
 
- console.log ('parse initialize firing')
+  console.log ('parse initialize firing')
 
- var Contact = Parse.Object.extend('Contact');
+  var Contact = Parse.Object.extend('Contact');
 
- var contact = new Contact();
+  var contact = new Contact();
 
- contact.set('name', Matt Hartley);
+  contact.set("contactName", "Matt Hartley");
+  contact.set("contactCity", "Greenville");
+  contact.set("email", "mbhartley1490@gmail.com");
 
- contact.save();
+  //contact.save(null, {
+  //	success: function(contactList){
+  //	  console.log('New object created with objectID' + contact.id);
+  //	},
+  //	error: function(contactList, error){
+  //	  console.log('Failed to create new object, with error code' + error.description);	
+  //	}
+
+  //});
 
 })
-
-
- var name = $('input[name=name]');
- var city = $('input[city=city]');
- var email = $('input[email=email]');
-
