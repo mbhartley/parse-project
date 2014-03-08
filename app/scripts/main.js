2a8668f5ba663632e98f8fbf2b7e146a618d3b47
Matt Hartley <mbhartley1490@gmail.com>
2014-03-08 12:45:41 -0500

    cleaned up and added comments

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,14 @@
 $(function(){
  
   Parse.initialize("75BmfRTZaHEhjFi2LGcwwpBSs9R2lavh3hDCwQtd", "j2gNcLny1ubShHclT3cPIA6PH06SDYRolgk4mflU");
-
-  console.log ('parse initialize firing');
-
+  
+  /*on page load, instantiate a new ContactListCollection*/
   window.contactList = new ContactListCollection();
 
+  /*on page load, fetch the collection from Parse cloud server
+  and, on successful fetch, instantiate a new ContactView,
+  passing the contactList model through the view; this will load 
+  the currently saved data from Parse*/
     contactList.fetch({
     success:function(){
       contactList.each(function(contactList){
@@ -14,25 +17,25 @@ $(function(){
     }
   })
 
-//On click: saveContact function is called
+//On click of submit button, after input fields are filled in: saveContact function is called
   $('.submit').click(function() {
     saveContact();
   })
-//On click: capture the value of input fields- name, city, email
-//  then set them as values of the object Contact
+//also, on click of submit button:
   function saveContact() {
-    //var Contact = Parse.Object.extend("Contact");
+//instantiate Parse.Object.extend (same as a model in Backbone)
     var contact = new Contact;
-
+//capture the values of the input fields via JQuery awesomeness
     var name = $('.name').val();
     var city = $('.city').val();
     var email = $('.email').val();
-  
+//set the values of input fields as values in our contact object instantiated 2 steps above
     contact.set("name", name);
     contact.set("city", city);
     contact.set("email", email);
-//On click: save the data on Parse cloud sever; on success, clear the input values
-//  and log success message; on fail, log error message
+
+/*Save function: on successful save: log success message, instantiate ContactView and pass in the model,
+clear each of the input values; if error, log error message*/
   contact.save(null, {
     success: function(contactList){
       console.log('New object created with objectID' + contact.id);
--- app/scripts/object-collection.js
+++ app/scripts/object-collection.js
@@ -1,3 +1,6 @@
+/*set up a constructor for a Parse object (think Backbone Model)
+and also set up a Collection for said object*/
+
 var Contact = Parse.Object.extend("Contact");
 
 var ContactListCollection = Parse.Collection.extend({
